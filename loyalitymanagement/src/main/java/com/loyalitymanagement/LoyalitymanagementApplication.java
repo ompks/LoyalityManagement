a2e1dcb966244b30e4cdf67157a2b5c3d587f1eb
ompks <ompks95@gmail.com>
2021-12-19 20:08:58 +0530

    initial commit

---
+++ loyalitymanagement/src/main/java/com/loyalitymanagement/LoyalitymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.loyalitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalitymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyalitymanagementApplication.class, args);
+	}
+
+}
---
+++ loyalitymanagement/src/test/java/com/loyalitymanagement/LoyalitymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyalitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyalitymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
